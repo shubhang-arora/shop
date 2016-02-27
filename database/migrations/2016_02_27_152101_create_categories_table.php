69f96e71b2b58b05b9c807fc4ab2e457fd0cd026
shubhang-arora <sa287@snu.edu.in>
2016-02-27 23:24:04 +0530

    Registration Form

---
+++ database/migrations/2016_02_27_152101_create_categories_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('category_user', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->integer('category_id')->unsigned()->index();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,16 +9,32 @@
     {!! Form::label('user_name','User Name:') !!}
     {!! Form::text('user_name',null) !!}
 
+    {!! Form::label('email','Email') !!}
+    {!! Form::text('email',null) !!}
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+    {!! Form::label('location','Location') !!}
+    {!! Form::text('location',null) !!}
 
+    {!! Form::label('city','City') !!}
+    {!! Form::text('city',null) !!}
 
+    {!! Form::label('state','State') !!}
+    {!! Form::text('state',null) !!}
 
+    {!! Form::label('phone','Contact') !!}
+    {!! Form::text('phone',null) !!}
 
+    {!! Form::label('amount_paid','Amount Paid') !!}
+    {!! Form::text('amount_paid',null) !!}
 
+    {!! Form::label('premium_shop','Premium Shop') !!}
+    {!! Form::checkbox('premium_shop',null) !!}
+
+    {!! Form::label('description','Description') !!}
+    {!! Form::textarea('description',null) !!}
+
+    {!! Form::label('admin_password','Admin Password') !!}
+    {!! Form::password('admin_password',null) !!}
     <div>
         <button type="submit">Register</button>
     </div>
