6e2ebbc19b4e98450d186ee56d84529c913ff520
Sahil Sharma <sahilsharma011@users.noreply.github.com>
2016-05-01 23:58:27 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<!--[if IE]><![endif]-->
+<!--[if IE]>
+<![endif]-->
 <!--[if IE 8 ]><html dir="ltr" lang="en" class="ie8"><![endif]-->
 <!--[if IE 9 ]><html dir="ltr" lang="en" class="ie9"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!-->
