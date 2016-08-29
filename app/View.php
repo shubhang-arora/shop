182521467aba22eadd55ae8784b640a18fddbabf
shubhang-arora <sa287@snu.edu.in>
2016-08-29 23:12:59 +0530

    Shop views

---
+++ app/View.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class View extends Model
+{
+    protected $fillable = [
+        'shop_id',
+        'ip'
+    ];
+
+
+}
---
+++ database/migrations/2016_08_29_230554_create_views_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->integer('shop_id')->unsigned();
+            $table->timestamps();
+
+
+            $table->foreign('shop_id')
+                ->references('id')
+                ->on('shops')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('views');
+    }
+}
